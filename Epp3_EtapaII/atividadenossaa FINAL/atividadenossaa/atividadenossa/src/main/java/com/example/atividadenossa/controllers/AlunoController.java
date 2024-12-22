cf726b7791f9774ca1784be17d879d2173323fa3
gabriel <gabrieltertoprezoto@gmail.com>
2024-12-21 21:08:12 -0300

    api-java-spring-web

---
+++ Epp3_EtapaII/atividadenossaa FINAL/atividadenossaa/atividadenossa/src/main/java/com/example/atividadenossa/controllers/AlunoController.java
@@ -0,0 +1,45 @@
+package com.example.atividadenossa.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.atividadenossa.entidades.Aluno;
+import com.example.atividadenossa.repositories.AlunoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class AlunoController {
+    
+    @Autowired
+    private AlunoRepository repository;
+
+    @GetMapping
+    public List<Aluno> findAll() {
+        List<Aluno> result = repository.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Aluno findById(@PathVariable Long id) {
+        Aluno result = repository.findById(id).get();
+        return result;
+    }
+
+    @PostMapping
+    public Aluno insert(@RequestBody Aluno aluno) {
+        Aluno result = repository.save(aluno);
+        return result;
+    }
+    
+
+
+}
+
---
+++ Epp3_EtapaII/atividadenossaa FINAL/atividadenossaa/atividadenossa/src/test/java/com/example/atividadenossa/AtividadenossaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadenossa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadenossaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
